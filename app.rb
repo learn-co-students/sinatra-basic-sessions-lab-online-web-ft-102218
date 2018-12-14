19aaf4b40b79a00468061b3a995f73989e3c7f84
kodyclemens <kody.clemens@gmail.com>
2018-12-14 18:03:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, 'yoda'
+end
+
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @params = params
+    session[:item] = params[:item]
+    erb :checkout
+  end
 end
